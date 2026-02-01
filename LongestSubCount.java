172c320eb1d690447d8f6d4f54b0a8af9149b541
HarshitaKoshta <harshitakoshta2111@gmail.com>
2026-02-01 13:28:21 +0530

    longest count

---
+++ LongestSubCount.java
@@ -0,0 +1,22 @@
+public class LongestSubCount {
+    public static void main(String[] args) {
+        int[] arr = {10, 5, 2, 7, 1, 9};
+        int k = 15;
+
+        int maxLen = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            int sum = 0;
+
+            for (int j = i; j < arr.length; j++) {
+                sum += arr[j];
+
+                if (sum == k) {
+                    maxLen = Math.max(maxLen, j - i + 1);
+                }
+            }
+        }
+
+        System.out.println(maxLen);
+    }
+}
