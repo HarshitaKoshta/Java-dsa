8652bcc50ecb868f793a1d6985e9738beb6a5c1a
HarshitaKoshta <harshitakoshta2111@gmail.com>
2026-03-10 20:03:51 +0530

    array list

--- ArrayLists.java
+++ ArrayLists.java
@@ -1,5 +1,4 @@
-
-import java.util.ArrayList;
+import java.util.*;
 
 public class ArrayLists {
     public static void main(String[] args) {
@@ -18,5 +17,43 @@ public class ArrayLists {
         for(int i =0;i<list.size();i++){
             System.out.println(list.get(i));
         }
+        System.out.println(list.contains(3));
+ 
+        int s=list.get(0);
+        int l = list.get(0);
+        for(int num : list){
+            if(num>l){
+                l=num;
+            }
+            if(num<s){
+                s=num;
+            }
+        }
+        System.out.println(l);
+        System.out.println(s);
+
+        int sum=0;
+        for(int num : list){
+            sum+=num;
+        }
+        System.out.println(sum);
+
+        int avg = sum/list.size();
+        System.out.println(avg);
+
+        for(int i=list.size()-1;i>=0;i--){
+            System.out.println(list.get(i));
+        }
+        int count=0;
+        for(int num : list){
+            if(num%2==0){
+                count++;
+            }
+        }
+        System.out.println(count);
+        Collections.sort(list);
+        System.out.println(list);
+        Collections.sort(list,Collections.reverseOrder());
+        System.out.println(list);
     }
 }
