7290f7e4f4d1e859922c961eee7a00cf83dc2394
HarshitaKoshta <harshitakoshta2111@gmail.com>
2026-03-13 23:40:24 +0530

    arr

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -1,30 +1,24 @@
 package Arrays;
-import java.util.*;
+
+import java.util.HashMap;
 
 public class TwoSum {
 
-    public static int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        
-        for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-
-            if (map.containsKey(complement)) {
-                return new int[] { map.get(complement), i };
-            }
-
-            map.put(nums[i], i);
-        }
-
-        return new int[] {};
-    }
-
     public static void main(String[] args) {
-        int[] nums = {2, 7, 11, 15};
-        int target = 9;
+        int[] arr = {2,7,23,9,5,6};
+        int tar = 16;
+         HashMap<Integer, Integer> map = new HashMap<>();
 
-        int[] result = twoSum(nums, target);
+        for(int i = 0; i < arr.length; i++) {
 
-        System.out.println(result[0] + ", " + result[1]);
+            int c = tar - arr[i];
+
+            if(map.containsKey(c)) {
+                System.out.println(map.get(c) + " " + i);
+                return;
+            }
+            map.put(arr[i], i);
+        }
+        System.out.println("No two elements sum up to the target.");
     }
 }
