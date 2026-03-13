7dd787830a5a749b5bf90f7bafb3c40c2d20a26a
HarshitaKoshta <harshitakoshta2111@gmail.com>
2026-03-13 09:02:42 +0530

    array

--- Arrays/LeadersInArr.java
+++ Arrays/LeadersInArr.java
@@ -1,16 +1,26 @@
+package Arrays;
+import java.util.*;
+
 public class LeadersInArr {
     public static void main(String[] args) {
+
         int[] arr = {16, 17, 4, 3, 5, 2};
         int n = arr.length;
-        System.out.println("Leaders in the array:");
-      int maxA = arr[n-1];
-      System.out.println(maxA);
 
-      for(int i=n-2;i>=0;i--){
-        if(arr[i]>maxA){
-            maxA = arr[i];
-            System.out.println(maxA+" ");
+        ArrayList<Integer> leaders = new ArrayList<>();
+
+        int maxA = arr[n-1];
+        leaders.add(maxA);
+
+        for(int i = n-2; i >= 0; i--){
+            if(arr[i] > maxA){
+                maxA = arr[i];
+                leaders.add(maxA);
+            }
         }
-      }
+
+        Collections.reverse(leaders);
+
+        System.out.println(leaders);
     }
 }
